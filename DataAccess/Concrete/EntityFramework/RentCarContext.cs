383cbc66afe4d99d2e1281359dce5a9760f408ce
Fatma <Fatma@DESKTOP-Q3FGQH0>
2021-04-07 15:02:56 +0300

    10.Gün

--- DataAccess/Concrete/EntityFramework/RentCarContext.cs
+++ DataAccess/Concrete/EntityFramework/RentCarContext.cs
@@ -1,37 +1,40 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
    public class RentCarContext : DbContext
     {
+        public class RentcarContext : DbContext
+        {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Rentcar;Trusted_Connection=true");
+            }
+            public DbSet<Cer> Cars { get; set; }
+            public DbSet<Brand> Brands { get; set; }
+            public DbSet<Color> Colors { get; set; }
+            public DbSet<Users> Users { get; set; }
+            public DbSet<Customer> Customers { get; set; }
+            public DbSet<Rentals> Rentals { get; set; }
+
+        }
+
+
+    }
+
+
+}
+;
+
         public object Car { get; internal set; }
         public object Cars { get; internal set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server = (localdb)\MSSQLLocalDb;Database = RentCar; Trusted_Connection=true"");
-        }
-
-        public DbSet<Car> Cars { get; set; } 
-
-        public DbSet<Brand> Brands { get; set; }
-
-        public DbSet<Color> Colors { get; set; }
-
-        public DbSet<Users>  Users { get; set; }
-
-        public DbSet<Customers> Customers { get; set; }
-
-        public DbSet <Rental> Rentals { get; set; }
-       
-
-     } 
-
-    
-}
-
-
-}
+            optionsBuilder.UseSqlServer(ConnectionString;
+        private const string ConnectionString = (V
